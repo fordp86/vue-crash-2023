a81960db910fec341797bd82d829b7181bd0922e
Ford Henley <fordp86@gmail.com>
2023-08-18 15:38:17 -0500

    Updated API Url
    add live backend url

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   devServer: {
     proxy: {
       "^/api": {
-        target: "http://localhost:5000",
+        target: "https://taskapp-jsonserver-f5efffa85a40.herokuapp.com",
         changeOrigin: true,
         logLevel: "debug",
         pathRewrite: { "^/api": "/" },

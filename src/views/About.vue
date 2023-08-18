ec8e81f1da52bed9cf3664660589d5e5eff835dc
Ford Henley <fordp86@gmail.com>
2023-08-18 12:50:44 -0500

    Added Routing
    Added routing and completed the project

---
+++ src/views/About.vue
@@ -0,0 +1,9 @@
+<template>
+  <h3>Version 1.0.0</h3>
+  <div>
+    <a target="_blank" href="https://github.com/fordp86/traversy-vue"
+      >View Source on Github</a
+    >
+  </div>
+  <router-link to="/">Go Back</router-link>
+</template>
